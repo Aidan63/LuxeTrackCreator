b167f2abda5ad73d81ae232682dac6d53073af09
Aidan Lee <aidan.lee63@gmail.com>
2017-06-28 00:19:45 +0100

    Curves will not have skeleton or tangent drawers by default

--- src/utils/SegmentConstructor.hx
+++ src/utils/SegmentConstructor.hx
@@ -10,8 +10,6 @@ class SegmentConstructor
         entity.add(new components.curves.LinearCurve(_points));
         entity.add(new components.Neighbours());
         entity.add(new components.CurvePoints());
-        entity.add(new components.RenderSkeleton());
-        entity.add(new components.RenderTangents());
         entity.add(new components.CurveTileRenderer());
 
         return entity;
@@ -23,8 +21,6 @@ class SegmentConstructor
         entity.add(new components.curves.QuadraticCurve(_points));
         entity.add(new components.Neighbours());
         entity.add(new components.CurvePoints());
-        entity.add(new components.RenderSkeleton());
-        entity.add(new components.RenderTangents());
         entity.add(new components.CurveTileRenderer());
 
         return entity;
@@ -36,8 +32,6 @@ class SegmentConstructor
         entity.add(new components.curves.CubicCurve(_points));
         entity.add(new components.Neighbours());
         entity.add(new components.CurvePoints());
-        entity.add(new components.RenderSkeleton());
-        entity.add(new components.RenderTangents());
         entity.add(new components.CurveTileRenderer());
 
         return entity;
