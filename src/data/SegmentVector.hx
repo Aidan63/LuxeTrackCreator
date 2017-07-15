d0160eca7f8fa8bec4e6049de5dc0b72e4142ae7
Aidan Lee <aidan.lee63@gmail.com>
2017-07-15 18:57:29 +0100

    SegmentVector now keeps an internal luxe vector up to date.
    The getLuxeVector function will not longer return a new luxe vector. Instead it will return a reference to one stored and kept up to date by the SegmentVector.

--- src/data/SegmentVector.hx
+++ src/data/SegmentVector.hx
@@ -3,44 +3,73 @@ package data;
 import luxe.Vector;
 
 /**
- Vector class for segments.
- Contains only x and y class members for cleaner serialization.
- 
- Contains functions for converting to and from Luxe vector.
+ * Vector class for segments.
+ * Contains only x and y class members for cleaner serialization.
+ * 
+ * Contains functions for converting to and from Luxe vector.
  */
 class SegmentVector
 {
     /**
-     The x component of the vector.
+     * The x component of the vector.
      */
-    public var x : Float;
+    @:isVar public var x(get, set) : Float;
 
     /**
-     The y component of the vector.
+     * The y component of the vector.
      */
-    public var y : Float;
+    @:isVar public var y(get, set) : Float;
+
+    /**
+     * Luxe vector instance containing the same x and y value as this segment vector.
+     */
+    private var luxeVector : Vector;
 
     public function new(_x : Float = 0, _y : Float = 0)
     {
+        luxeVector = new Vector();
+
         x = _x;
         y = _y;
     }
 
-    /**
-     Creates a luxe vector from this segment vector
-     
-     @return Luxe vector containing the same x and y component values.
-     */
-    public function getLuxeVector() : Vector
+    function set_x(_x : Float)
     {
-        return new Vector(x, y);
+        luxeVector.x = _x;
+        return x = _x;
+    }
+
+    function get_x()
+    {
+        return x;
+    }
+
+    function set_y(_y : Float)
+    {
+        luxeVector.y = _y;
+        return y = _y;
+    }
+
+    function get_y()
+    {
+        return y;
     }
 
     /**
-     Creates a segment vector from the provided luxe vector.
+     * Creates a luxe vector from this segment vector
+     *
+     * @return Luxe vector containing the same x and y component values.
+     */
+    public function getLuxeVector() : Vector
+    {
+        return luxeVector;
+    }
 
-     @param _vector The luxe vector to get values from.
-     @return segment vector containing the same x and y as the luxe vector.
+    /**
+     * Creates a segment vector from the provided luxe vector.
+     *
+     * @param _vector The luxe vector to get values from.
+     * @return segment vector containing the same x and y as the luxe vector.
      */
     public static function fromLuxeVector(_vector : Vector) : SegmentVector
     {
